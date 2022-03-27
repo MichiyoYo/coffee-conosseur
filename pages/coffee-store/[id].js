3a86f60bc6cdadf211ee1887fe875e72103f6d16
Cristina Lester <cristina.deelester@gmail.com>
2022-03-27 16:10:40 -0700

    adding dynamic routes

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function CoffeeStore(props) {
-  return <div> this is the coffee store</div>;
+  return <div> this is the coffee store page </div>;
 }
 
 export default CoffeeStore;
