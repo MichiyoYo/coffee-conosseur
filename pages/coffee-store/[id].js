dc55af50a49f90fcf8080686a430b0f9dde5927e
Cristina Lester <cristina.deelester@gmail.com>
2022-03-27 16:20:23 -0700

    added link back to home

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,10 +1,18 @@
 import React from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 function CoffeeStore(props) {
   const { query } = useRouter();
 
-  return <div> {query.id} </div>;
+  return (
+    <div>
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+      {query.id}{" "}
+    </div>
+  );
 }
 
 export default CoffeeStore;
