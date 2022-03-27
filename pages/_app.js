809ddf216e9e25e55731c8c1a10b9cd098333dbd
Cristina Lester <cristina.deelester@gmail.com>
2022-03-27 08:33:58 -0700

    clean up

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import styles from "../styles/Footer.module.css";
+
+const Footer = () => {
+  console.log(styles);
+  return (
+    <footer className={styles.footer}>
+      <p>Cristina Lester &copy; 2022</p>
+    </footer>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
